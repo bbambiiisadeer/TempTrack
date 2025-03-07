6a6aa90587486395c056e95dbc757e5e9ef82510
bbam <nunnapats1822@gmail.com>
2025-03-07 19:23:27 +0700

    ปรับปุ่ม signin

--- frontend/src/pages/firstpage.tsx
+++ frontend/src/pages/firstpage.tsx
@@ -43,7 +43,7 @@ export default function FirstPage() {
           <span className="text-black">|</span>
           <Link
             to="/signin"
-            className="font-bold px-6 py-2 bg-[#BBFC4C] text-black font-md rounded-lg shadow-lg hover:bg-[#F1FD46]"
+            className="font-bold px-6 py-2 bg-[#BBFC4C] text-black font-md rounded-lg shadow-lg hover:bg-[#F1FD46] transition duration-30"
           >
             Sign in
           </Link>
