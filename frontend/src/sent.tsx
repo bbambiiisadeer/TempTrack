ec5e4bd007f62a033b5c03c5933ac8caf835930a
bbam <nunnapats1822@gmail.com>
2025-12-17 20:42:58 +0700

    Format date output in Incoming and Sent pages: change to DD-MM-YYYY format

--- frontend/src/sent.tsx
+++ frontend/src/sent.tsx
@@ -139,7 +139,11 @@ function SentPage() {
 
   const formatDate = (dateString: string) => {
     const date = new Date(dateString);
-    return date.toLocaleDateString("en-CA");
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    
+    return `${day}-${month}-${year}`;
   };
 
   useEffect(() => {
